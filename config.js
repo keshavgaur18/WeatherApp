bf9d36aa7e4de641b5baa1ddd122e4e2c05282c7
keshavgaur18 <67282657+keshavgaur18@users.noreply.github.com>
2022-03-28 13:11:09 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+  openWeatherMap: {
+    BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+    SECRET_KEY: "56f03da65f70cf71b984fbe16b5ce0e6",
+  },
+};
+
+module.exports = constants;
